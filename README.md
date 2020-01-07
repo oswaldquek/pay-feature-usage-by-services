18aed6890778b2c3a9e9a8baa548b34f4b56edd2
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2020-01-07 11:28:42 +0000

    add database connection to connector db

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # pay-feature-usage-by-services
+
+Open tunnel to adminusers.
+
+Open tunnel connector database.
+
+Set environment variables PGDATABASE, PGPASSWORD, PGUSER in terminal.
+
+Run `PGUSER=$PGUSER PGPASSWORD=$PGPASSWORD npm start`
+
+Go to localhost:3000/graphql to start querying.
--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
+const { Client } = require('pg')
+const client = new Client({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: "connector",
+  port: 5432,
+  host: "localhost",
+  ssl: true
+})
 
 const graphQlSchema = require('./graphql/schema/index');
 const graphQlResolvers = require('./graphql/resolvers/index');
 
 const app = express();
+
+const testDbConnection = async function() {
+  try {
+    await client.connect()
+    const res = await client.query('SELECT NOW() as now')
+    console.log('res: ' + JSON.stringify(res.rows))
+    await client.end()
+  } catch (err) {
+    console.log(err)
+    throw err
+  }
+} 
 
 app.use(bodyParser.json());
 
@@ -17,5 +38,7 @@ app.use(
     graphiql: true
   })
 );
+
+testDbConnection()
 
 app.listen(3000);
